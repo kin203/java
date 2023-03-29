2800c56bca5c4cf8df02ece85c84fb3d33b49b91
kiên nguyễn <kien12342003@gmail.com>
2023-03-29 14:12:07 +0700

    add

---
+++ src/KiemTra.java
@@ -0,0 +1,8 @@
+public class KiemTra {
+    public static void main(String[] args) {
+        NhanVien nv=new NhanVien(01, "Nguyen Xuan Kien", 50000);
+        System.out.println(nv);
+        System.out.println("luong 1 nam cua be nhan vienla: "+nv.luongtheonam());
+        System.out.println("luong duoc tang cua nhan vien la: "+nv.tangluong());
+    }
+}
---
+++ src/NhanVien.java
@@ -0,0 +1,48 @@
+public class NhanVien {
+    private int manv;
+    private String hoten;
+    private double luong;
+    public NhanVien(int manv, String hoten, double luong){
+        super();
+        this.manv=manv;
+        this.hoten=hoten;
+        this.luong=luong;
+    }
+    public int getManv(){
+        return manv;
+    }
+    public void setManv(int manv){
+        this.manv=manv;
+    }
+    public String gethoten(){
+        return hoten;
+    }
+    public void sethoten(String hoten) {
+        this.hoten = hoten;
+    }
+
+    public double getluong() {
+        return luong;
+    }
+
+    public void setluong(double luong) {
+        this.luong = luong;
+    }
+
+    @Override
+    public String toString() {
+        return "NhanVien{" +
+                "manv=" + manv +
+                ", hoten='" + hoten + '\'' +
+                ", luong=" + luong +
+                '}';
+    }
+
+    public  double luongtheonam(){
+        return this.luong*12;
+    }
+    public double tangluong(){
+        return this.luong=this.luong*0.5;
+    }
+
+}
